5f07534489a926d31e54d2fdd12b9eaa68cfc789
Vedansh Jain <vedansh.code@gmail.com>
2023-12-31 18:51:38 +0530

    first commit

---
+++ assets/js/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const api_key = 'f722f5f4f8d180aa23cfb037e2353a03';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+/**
+ * fetch data from a server using the `url` and passes
+ * the result in JSON data to a `callback` function,
+ * along with an optional parameter if has `optionalParam`.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
